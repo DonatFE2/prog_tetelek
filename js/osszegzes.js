841e9683f6a74c58b65f7809445db9b57463e473
DonatFE2 <114178937+DonatFE2@users.noreply.github.com>
2022-09-27 19:00:39 +0200

    Update osszegzes.js

--- js/osszegzes.js
+++ js/osszegzes.js
@@ -1,10 +1,11 @@
 function osszegzes(){
     let osszeg = 0;
-
+    let span = document.getElementById("osszeg");
+    
     for(let i=0; i<tomb.length; i++)
         osszeg += tomb[i];
 
-    document.getElementById("osszeg").innerHTML = osszeg;
+    span.innerHTML = osszeg;
 }
 
 /* Átlagolás: HÁZI! */
